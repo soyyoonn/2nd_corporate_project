bd35b585b12a632a7c08ccd7733413cae77c540d
soyyoonn <warminhap@gmail.com>
2023-04-08 19:24:05 +0900

    오류,ui 수

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,100 +31,159 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btn_go = new System.Windows.Forms.Button();
+            this.lbl_recommendedTime = new System.Windows.Forms.Label();
+            this.tbx_medi = new System.Windows.Forms.TextBox();
+            this.cmb_patient = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(100, 66);
+            this.label1.Location = new System.Drawing.Point(100, 82);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 12);
+            this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "환자 정보";
+            this.label1.Text = "환자명";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(100, 119);
+            this.label2.Location = new System.Drawing.Point(83, 122);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.Size = new System.Drawing.Size(91, 24);
             this.label2.TabIndex = 2;
-            this.label2.Text = "마취량";
+            this.label2.Text = "마취제 용량\r\n(숫자 입력요망)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(100, 170);
+            this.label3.Location = new System.Drawing.Point(100, 169);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.Size = new System.Drawing.Size(53, 12);
             this.label3.TabIndex = 4;
-            this.label3.Text = "타이머";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(252, 215);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(57, 12);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "실행 시간";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(252, 66);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(97, 12);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "이름 또는 아이디";
+            this.label3.Text = "권장시간";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(252, 119);
+            this.label6.Location = new System.Drawing.Point(316, 125);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(87, 12);
+            this.label6.Size = new System.Drawing.Size(23, 12);
             this.label6.TabIndex = 7;
-            this.label6.Text = "마취투여량 mg";
+            this.label6.Text = "mg";
             // 
-            // button1
+            // btn_go
             // 
-            this.button1.Location = new System.Drawing.Point(174, 268);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "실행 버튼";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btn_go.Location = new System.Drawing.Point(177, 250);
+            this.btn_go.Name = "btn_go";
+            this.btn_go.Size = new System.Drawing.Size(75, 23);
+            this.btn_go.TabIndex = 8;
+            this.btn_go.Text = "실행";
+            this.btn_go.UseVisualStyleBackColor = true;
+            this.btn_go.Click += new System.EventHandler(this.btn_go_Click);
+            // 
+            // lbl_recommendedTime
+            // 
+            this.lbl_recommendedTime.Location = new System.Drawing.Point(187, 169);
+            this.lbl_recommendedTime.Name = "lbl_recommendedTime";
+            this.lbl_recommendedTime.Size = new System.Drawing.Size(123, 12);
+            this.lbl_recommendedTime.TabIndex = 9;
+            this.lbl_recommendedTime.Text = "miniute";
+            this.lbl_recommendedTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tbx_medi
+            // 
+            this.tbx_medi.Location = new System.Drawing.Point(189, 122);
+            this.tbx_medi.Name = "tbx_medi";
+            this.tbx_medi.Size = new System.Drawing.Size(121, 21);
+            this.tbx_medi.TabIndex = 12;
+            this.tbx_medi.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.tbx_medi.TextChanged += new System.EventHandler(this.tbx_medi_TextChanged);
+            this.tbx_medi.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbx_medi_KeyPress);
+            // 
+            // cmb_patient
+            // 
+            this.cmb_patient.FormattingEnabled = true;
+            this.cmb_patient.Location = new System.Drawing.Point(189, 79);
+            this.cmb_patient.Name = "cmb_patient";
+            this.cmb_patient.Size = new System.Drawing.Size(121, 20);
+            this.cmb_patient.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(187, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "졸음경보기";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(103, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 12);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "타이머";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(121, 21);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(252, 170);
+            this.label7.Location = new System.Drawing.Point(316, 169);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(57, 12);
-            this.label7.TabIndex = 9;
-            this.label7.Text = "시작 시간";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "분";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(316, 206);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 12);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "분";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(431, 330);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmb_patient);
+            this.Controls.Add(this.tbx_medi);
+            this.Controls.Add(this.lbl_recommendedTime);
+            this.Controls.Add(this.btn_go);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,11 +193,16 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_go;
+        private System.Windows.Forms.Label lbl_recommendedTime;
+        private System.Windows.Forms.TextBox tbx_medi;
+        private System.Windows.Forms.ComboBox cmb_patient;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,117 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace cnn_project
 {
     public partial class Form1 : Form
     {
+        MySqlConnection connection = new MySqlConnection("server=10.10.21.102;Database=medical_details;Uid=manager;Pwd=0000");
+
         public Form1()
         {
             InitializeComponent();
+            cmb_patient.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void btn_go_Click(object sender, EventArgs e)
+        {
+            if (cmb_patient.SelectedItem == null)
+                MessageBox.Show("환자 리스트를 선택해주세요");
+            else if(tbx_medi.Text.Length == 0)
+            {
+                MessageBox.Show("투여 마취제 용량을 작성해주세요");
+            }
+            else
+            {
+                int dose = int.Parse(tbx_medi.Text);
+                string insertQuery = $"INSERT INTO medical_records(anesthetic_dose) VALUES({dose})";
+                connection.Open();
+                MySqlCommand command = new MySqlCommand(insertQuery, connection);
+                connection.Close();
+
+                // form2로 이동
+                this.Hide();
+                Form2 timer_form = new Form2();
+                timer_form.ShowDialog();
+                this.Close();
+            }
+        }
+
+        private void tbx_medi_TextChanged(object sender, EventArgs e)
+        {
+
+            // 현재 시각 뜸
+            // string time = DateTime.Now.ToString("yy년 MM월 dd일 HH:mm:ss");
+            
+            // 마취제 양에 따라서 타이머 권장 시간을 작성
+            if (tbx_medi.Text.Length != 0)
+            {
+                string recommendedTime = Convert.ToString(int.Parse(tbx_medi.Text) * 10);
+                lbl_recommendedTime.Text = recommendedTime;
+            }
+
+            //string timeset = tbx_medi.Text; // 변환할 문자열
+            //int num; // 정수로 변환된 값을 저장할 변수
+
+            //if (int.TryParse(timeset, out num))
+            //{
+            //    // timeset 변수의 문자열이 정수로 변환되었을 경우에 실행되는 코드
+            //    // num 변수에 정수 값이 할당됨
+            //    Console.WriteLine("정수로 변환된 값: " + num);
+            //}
+            //else
+            //{
+            //    // timeset 변수의 문자열이 정수로 변환되지 않았을 경우에 실행되는 코드
+            //    // 에러 처리 등을 수행할 수 있음
+            //    Console.WriteLine("입력 문자열의 형식이 잘못되었습니다.");
+            //}
+
+
+            //TimeSpan ts = new TimeSpan(0, num * 10, 0);
+            //DateTime time_num = Convert.ToDateTime(time);
+            //DateTime timer_result = time_num + ts;
+            //string timer_result2 = timer_result.ToString("yy년 MM월 dd일 HH:mm:ss");
+        }
+
+        private void tbx_medi_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //숫자와 백스페이스를 제외한 나머지를 바로 처리
+            if (!(char.IsDigit(e.KeyChar) || e.KeyChar == Convert.ToChar(Keys.Back)))
+            {
+                e.Handled = true;
+            }
+        }
+
+        // form을 열었을 때 실행(콤보박스에 환자 리스트 추가)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string selectQuery = "SELECT * FROM medical_details.patient_info";
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
+                MySqlDataReader table = cmd.ExecuteReader();
+
+                while (table.Read())
+                {
+                    Console.WriteLine("환자번호: {0}, 이름: {1}, 성별: {2}", table["patient_id"], table["name"], table["gender"]);
+                    cmb_patient.Items.Add(table["name"]);
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("실패");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
